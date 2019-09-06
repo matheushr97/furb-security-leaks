baf532a55aae37a8155acbb2c62429409e1b22fd
matheushr97 <matheushr97@gmail.com>
2019-09-05 21:43:02 -0300

    Commit inicial

---
+++ src/main/java/br/com/furb/lista5/q1/Main.java
@@ -0,0 +1,26 @@
+package br.com.furb.lista5.q1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/views/Lista5Q1.fxml"));
+			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/br/com/furb/lista5/q1/SampleController.java
@@ -0,0 +1,5 @@
+package br.com.furb.lista5.q1;
+
+public class SampleController {
+	
+}
